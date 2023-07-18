52bbbd8e077f4ea22270a5cbcc0d2b47974c17ab
reza <reza.pratama@concretejakarta.com>
2023-07-19 00:45:53 +0700

    reza-3 test reza-3 test

--- src/js/Pages/Front/Home.jsx
+++ src/js/Pages/Front/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
       {/* seo */}
       <h1 className="text-center absolute mx-auto opacity-0">
         Portfolio Reza Bagus Pratama
-        reza-3 test reza-3
+        reza-3 test reza-3 test
       </h1>
 
       {/* banner dekstop */}
